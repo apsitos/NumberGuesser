806fce58c37791c2ae91c88537e6a1be9a2fad88
Justin Wood <jwood11atx@gmail.com>
2016-10-12 17:44:00 -0600

    please work

--- javascript.js
+++ javascript.js
@@ -9,137 +9,137 @@ var theGuess;
 
 
 startButton.addEventListener("click", function() {
-  checkMinMax();
+ checkMinMax();
 });
 
 guessButton.addEventListener("click", function() {
-  document.querySelector("#resultMsg").innerText = "Your last guess was";
-  mainFunc();
-  disableClearBtn();
+ document.querySelector("#resultMsg").innerText = "Your last guess was";
+ mainFunc();
+ disableClearBtn();
 });
 
 clearButton.addEventListener("click", function() {
-  clearGuess();
-  disableClearBtn();
+ clearGuess();
+ disableClearBtn();
 });
 
 resetButton.addEventListener("click", function() {
-  resetGame();
+ resetGame();
 });
 
 
 inputField.addEventListener("keyup", function() {
-  if(inputField.value === ""){
-      clearButton.disabled = true;
-    } else {
-      clearButton.disabled = false;
-    };
+ if(inputField.value === ""){
+     clearButton.disabled = true;
+   } else {
+     clearButton.disabled = false;
+   };
 });
 
 function disableClearBtn(){
-  if(inputField.value === ""){
-    clearButton.disabled = true;
-  } else {
-    clearButton.disabled = false;
-  };
+ if(inputField.value === ""){
+   clearButton.disabled = true;
+ } else {
+   clearButton.disabled = false;
+ };
 }
 
 function mainFunc(){
-  if(inputCheck()) {
-    compareGuess();
-    guessDisplay();
-  } else {
-    badInputDisplay();
-  }
+ if(inputCheck()) {
+   compareGuess();
+   guessDisplay();
+ } else {
+   badInputDisplay();
+ }
 };
 
 function compareGuess() {
-  var input = getGuess();
-  if(input === theGuess){
-    correctNum();
-  }
-  else
-    input > theGuess ? tooHigh() : tooLow();
+ var input = getGuess();
+ if(input === theGuess){
+   correctNum();
+ }
+ else
+   input > theGuess ? tooHigh() : tooLow();
 };
 
 function randoNum() {
-  return Math.floor(Math.random()*getMax())+getMin();
+ return Math.floor(Math.random()*(getMax()-getMin())+getMin());
 }
 
 function clearGuess() {
-  return document.getElementById("inputbox").value = "";
+ return document.getElementById("inputbox").value = "";
 };
 
 function resetGame() {
-  location.reload();
+ location.reload();
 }
 
 function getGuess(){
-  return Number(document.getElementById("inputbox").value);
+ return Number(document.getElementById("inputbox").value);
 }
 
 function getMin() {
-  return Number(document.getElementById("inputboxMin").value);
+ return Number(document.getElementById("inputboxMin").value);
 }
 
 function getMax() {
-  return Number(document.getElementById("inputboxMax").value);
+ return Number(document.getElementById("inputboxMax").value);
 }
 
 function tooHigh() {
-  return document.querySelector("#textResult").innerText = "That is too high";
+ return document.querySelector("#textResult").innerText = "That is too high";
 };
 
 function tooLow() {
-  return document.querySelector("#textResult").innerText = "That is too low"
+ return document.querySelector("#textResult").innerText = "That is too low"
 }
 
 function correctNum() {
-  var newMin  = getMin() - 10;
-  var newMax = getMax() + 10;
-  document.getElementById("inputboxMin").value = newMin;
-  document.getElementById("inputboxMax").value = newMax;
-  guessButton.style.display = "none";
-  startButton.style.display = "inline-block";
+ var newMin  = getMin() - 10;
+ var newMax = getMax() + 10;
+ document.getElementById("inputboxMin").value = newMin;
+ document.getElementById("inputboxMax").value = newMax;
+ guessButton.style.display = "none";
+ startButton.style.display = "inline-block";
 
-  return document.querySelector("#textResult").innerText = "That is correct! Now try to guess the number between the new range!";
+ return document.querySelector("#textResult").innerText = "That is correct! Try to guess the number between the new range!";
 };
 
 function guessDisplay() {
-  document.querySelector("h2").innerText = getGuess();
+ document.querySelector("h2").innerText = getGuess();
 };
 
 function badInputDisplay() {
-  document.querySelector("h2").innerText = "";
-  document.querySelector("#textResult").innerText = "NOT a number between " + getMin() + "-" + getMax() + "."
+ document.querySelector("h2").innerText = "";
+ document.querySelector("#textResult").innerText = "NOT a number between " + getMin() + "-" + getMax() + "."
 };
 
 function badMinMaxDisplay() {
-  document.getElementById("errorMinMaxMsg").style.display = "block";
+ document.getElementById("errorMinMaxMsg").style.display = "block";
 }
 
 function addResultSection() {
-  document.getElementById("result").style.display = "block";
-  document.getElementById("clear-btn").style.display = "inline-block";
-  document.getElementById("errorMinMaxMsg").style.display = "none";
+ document.getElementById("result").style.display = "block";
+ document.getElementById("clear-btn").style.display = "inline-block";
+ document.getElementById("errorMinMaxMsg").style.display = "none";
 };
 
 function checkMinMax() {
-  if(getMin() !== "" && getMax() !== "" && getMin()<getMax()) {
-    guessButton.style.display = "inline-block";
-    startButton.style.display = "none";
-    errorMinMaxMsg.style.display = "none";
-    inputboxMin.disabled = true;
-    inputboxMax.disabled = true;
-    inputbox.disabled = false;
-    addResultSection();
-    return theGuess = randoNum();
-  } else {
-    badMinMaxDisplay();
-  }
+ if(getMin() !== "" && getMax() !== "" && getMin()<getMax()) {
+   guessButton.style.display = "inline-block";
+   startButton.style.display = "none";
+   errorMinMaxMsg.style.display = "none";
+   inputboxMin.disabled = true;
+   inputboxMax.disabled = true;
+   inputbox.disabled = false;
+   addResultSection();
+   return theGuess = randoNum();
+ } else {
+   badMinMaxDisplay();
+ }
 };
 
 function inputCheck() {
-  var input = getGuess();
-  return (input>getMin()-1 && input<getMax()+1);
+ var input = getGuess();
+ return (input>getMin()-1 && input<getMax()+1);
 };
